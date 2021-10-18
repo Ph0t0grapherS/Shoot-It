854f0578fe5de2be7658ccf2998b63335455c957
Mazzurka <ermachkova.mari@bk.ru>
2021-10-18 20:37:59 +0300

    Шрифт, цвет фона, подчёркивание

--- lib/main.dart
+++ lib/main.dart
@@ -5,41 +5,46 @@ import 'package:flutter/material.dart';
 void main() => runApp(ShootIt());
 
 class ShootIt extends StatelessWidget {
+  const ShootIt({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(
-        color: Colors.blueGrey.shade900,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            //Название:
-            Container(
-              alignment: Alignment.topLeft,
-              padding: const EdgeInsets.only(left: 28, top: 18),
-              child: Text('Shoot It',
-                style: TextStyle(fontSize: 32, color: Colors.blueGrey.shade500),
-              ),
-            ),
-            //Кнопка меню:
-            Container(alignment:
-              Alignment.topRight,
-              padding: const EdgeInsets.only(top: 18, right: 28),
-              child: TextButton(
-                  child: const Icon(Icons.menu_rounded, size: 38, color: Colors.white),
-                  onPressed: () => {Menu()}
-              )
-            )
-          ]
-        )
-      )
-    );
+        home: Container(
+            color: const Color(0xFF07102F),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //Название:
+                  Container(
+                    alignment: Alignment.topLeft,
+                    padding: const EdgeInsets.only(left: 28, top: 20),
+                    child: const Text(
+                      'SHOOT IT',
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Color(0xFF505770),
+                          fontFamily: 'Roboto',
+                          decoration: TextDecoration.none),
+                    ),
+                  ),
+                  //Кнопка меню:
+                  Container(
+                      alignment: Alignment.topRight,
+                      padding: const EdgeInsets.only(top: 18, right: 28),
+                      child: TextButton(
+                          child: const Icon(Icons.menu_rounded,
+                              size: 38, color: Colors.white),
+                          onPressed: () => {const Menu()}))
+                ])));
   }
 }
 
 //Всплывающее меню:
 
 class Menu extends StatelessWidget {
+  const Menu({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -49,18 +54,13 @@ class Menu extends StatelessWidget {
             color: Colors.white,
             width: 40,
             height: 40,
-            child: Column(
-              children: [
-                Container(
+            child: Column(children: [
+              Container(
                   alignment: Alignment.topLeft,
                   padding: const EdgeInsets.only(left: 5, top: 5),
                   child: ElevatedButton(
-                    onPressed: (){},
-                    child: const Text("Попробуй что-то новое!")
-                )
-                )
-              ])
-        )
-    );
+                      onPressed: () {},
+                      child: const Text("Попробуй что-то новое!")))
+            ])));
   }
 }
