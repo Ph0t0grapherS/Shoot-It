9abd680c983100a58a6a20ac795ff9e630a8657d
Wolf-from-Cintra <chahir.arina@yandex.ru>
2021-10-19 14:35:03 +0300

    File hierarchy changes

---
+++ lib/files/pages/first_screen.dart
@@ -0,0 +1,26 @@
+/*import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+
+class First_screen extends StatelessWidget {
+  const First_screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Container(
+          decoration: const BoxDecoration(color: Colors.white),
+            alignment: Alignment.center,
+            padding: const EdgeInsets.only(left: 30, right: 30),
+            color: Colors.blueGrey.shade900,
+            child: ListView(
+              children: [
+                Center(
+                  child: Image.asset("assets/images/1.jpg", fit: BoxFit.cover),
+                ),
+              ],
+            )
+        )
+    );
+  }
+}*/
---
+++ lib/files/popup_menu.dart
@@ -0,0 +1,32 @@
+/*import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+//import 'pages/first_screen.dart';
+
+// Всплывающее меню:
+
+class Menu extends StatelessWidget {
+  const Menu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Container(
+            alignment: Alignment.topRight,
+            padding: const EdgeInsets.only(top: 18, right: 28),
+            color: const Color(0xFFe9edf5),
+            child: Column(
+                children: [
+                  Container(
+                    //alignment: Alignment.topLeft,
+                      padding: const EdgeInsets.only(left: 5, top: 5),
+                      child: ElevatedButton(
+                          onPressed: (){},
+                          child: const Text("shoot-it")
+                      )
+                  )
+                ])
+        )
+    );
+  }
+}*/
